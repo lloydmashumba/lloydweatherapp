7900f22de17a362d5fe63ba60fc539b1842a5087
lloyd <lloydmashumba@users.noreply.github.com>
2022-07-23 10:58:11 +0200

    created a forecast dynamic view

--- lloydweatherapp/controller/MainVC.swift
+++ lloydweatherapp/controller/MainVC.swift
@@ -13,11 +13,20 @@ class MainVC: UIViewController {
     
     @IBOutlet weak var currentTempView: NSLayoutConstraint!
     
+    @IBOutlet weak var mainForecastStack: UIStackView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
         self.view.backgroundColor = UIColor(named : "sea_cloudy_color")
+        
+        let days = ["Monday","Tuesday","Wednesday","Thursday","Friday"]
+        
+        
+        for i in 0...(days.count - 1) {
+            mainForecastStack.addArrangedSubview(ForecastView(frame: CGRect.zero,day: days[i],temperature: 45))
+        }
+        
     }
     
     override func viewWillAppear(_ animated: Bool) {
--- lloydweatherapp/view/ForecastView.swift
+++ lloydweatherapp/view/ForecastView.swift
@@ -7,27 +7,47 @@
 
 import UIKit
 
+//ForecastView is used to display weather forcast for future days in MainVC
 public class ForecastView : UIView {
     
     let foreCastDetailsStack = UIStackView()
-    var day : String!
-    var image : String!
-    var temperature : String!
+    private var day : String
+    private var image : String!
+    var temperature : Int
     
-    public override func draw(_ rect: CGRect) {
+    public init(frame: CGRect,day : String,temperature : Int) {
+        self.day = day
+        self.temperature = temperature
+        super.init(frame: frame)
+        createView()
+    }
+    
+    private func createView(){
+        addSubview(foreCastDetailsStack)
+        
+        foreCastDetailsStack.translatesAutoresizingMaskIntoConstraints = false
         
         foreCastDetailsStack.alignment = .fill
+        foreCastDetailsStack.distribution = .equalSpacing
         foreCastDetailsStack.leadingAnchor.constraint(equalTo: self.leadingAnchor).isActive = true
         foreCastDetailsStack.topAnchor.constraint(equalTo: self.topAnchor).isActive = true
         foreCastDetailsStack.bottomAnchor.constraint(equalTo: self.bottomAnchor).isActive = true
         foreCastDetailsStack.trailingAnchor.constraint(equalTo: self.trailingAnchor).isActive = true
         
-        foreCastDetailsStack.addSubview(forecastLabel(text: "Monday"))
-        foreCastDetailsStack.addSubview(forecastLabel(text: ""))
-        foreCastDetailsStack.addSubview(forecastLabel(text: "26"))
+        let forecastIconView = UIView()
         
-        self.addSubview(foreCastDetailsStack)
+        foreCastDetailsStack.addArrangedSubview(forecastLabel(text: day))
+        foreCastDetailsStack.addArrangedSubview(forecastIconView)
+        forecastIconSetup(view : forecastIconView,icon: "clear")
+        foreCastDetailsStack.addArrangedSubview(forecastLabel(text: "\(temperature)"))
+        
+        layoutSubviews()
     }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     
     private func forecastLabel(text value : String) -> UILabel{
         let label = UILabel()
@@ -36,7 +56,24 @@ public class ForecastView : UIView {
         return label;
     }
     
-    
+    private func forecastIconSetup(view : UIView,icon : String){
+        
+        
+        let iconView = UIImageView()
+        
+        
+        view.addSubview(iconView)
+        iconView.translatesAutoresizingMaskIntoConstraints = false
+        
+        iconView.image = UIImage(named: icon)
+        
+        iconView.centerYAnchor.constraint(equalTo: centerYAnchor).isActive = true
+        iconView.centerXAnchor.constraint(equalTo: centerXAnchor).isActive = true
+        iconView.widthAnchor.constraint(lessThanOrEqualToConstant: 60).isActive = true
+        iconView.heightAnchor.constraint(lessThanOrEqualToConstant: 60).isActive = true
+        
+        
+    }
     
 }
 
